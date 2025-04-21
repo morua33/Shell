4001a429b4bf7e13b23042307ccfab2651b53181
unknown <emerymorua33@gmail.com>
2025-04-21 18:40:06 -0500

    asdf

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,20 @@
 #include <sstream>
 #include <filesystem>
 
+std::string get_path(std::string command){
+  std::string path_env = std::getenv("PATH");
+  std::stringstream ss(path_env);
+  std::string path;
+  while(!ss.eof()) {
+      getline(ss, path, ':');
+      std::string abs_path = path + "/" + command;
+      if (std::filesystem::exists(abs_path)) {
+          return abs_path;
+      }
+  }
+  return "";
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -27,10 +41,17 @@ int main() {
         std::cout << cmd << " is a shell builtin" << std::endl;
       }
       else{
-        std::cout << cmd << ": not found\n";
-
+        
+        else{
+          std::string path = get_path(cmd);
+          if(path.empty()){
+            std::cout << cmd << ": not found\n";
+          }
+          else{
+            std::cout << input.substr(5) << " is " << path << std::endl;
+          }
+        }
       }
-
     
     }
     
